3963f786c7aca29f5a4620ce99c958f9195be761
Kuro <quangacon02@gmail.com>
2024-02-27 09:41:24 +0700

    [BE]: Set up routes

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,12 +1,23 @@
 import express, { Request, Response } from 'express';
-import fs from 'fs';
+import morgan from 'morgan';
+
+import videos from './routes/videos.js';
+
 
 const app = express();
 const port = 8000;
 
+
+// Middlewares
+app.use(morgan('tiny'));
+
+// Routes
+app.use("/videos", videos);
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ be/src/routes/videos.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from 'express';
+import { buffer } from 'stream/consumers';
+
+const videos = express.Router();
+
+videos.use("/full", express.static("public/videos/full",{cacheControl:true, maxAge: 31536000}));
+videos.use("/transcoded", express.static("public/videos/transcoded", {cacheControl:true, maxAge: 31536000,}));
+
+export default videos;
