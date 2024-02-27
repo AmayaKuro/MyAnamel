f8ad5a9e8ef454490a28064d2deb0f55754872f3
Kuro <quangacon02@gmail.com>
2024-02-27 09:59:50 +0700

    [FE]: Allow external image reference

--- fe/next.config.mjs
+++ fe/next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'http',
+                hostname: 'localhost',
+                port: '8000',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.amayakuro.id.vn',
+                port: '',
+            },
+        ],
+    },
+};
+// TODO: Need to add mp4 here??
 
 export default nextConfig;
