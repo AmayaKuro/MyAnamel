a4a23871a06f2beac6d476683614800601e2e702
Kuro <quangacon02@gmail.com>
2024-03-07 15:00:54 +0700

    [BE] Add CORS configuration

--- be/src/index.ts
+++ be/src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 import videos from './routes/videos.js';
+
+import  convertVideo  from './utils/ffmpeg.js';
 
 
 const app = express();
@@ -9,13 +12,21 @@ const port = 8000;
 
 
 // Middlewares
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    optionsSuccessStatus: 200
+}
+
 app.use(morgan('tiny'));
+app.use(cors(corsOptions));
+
 
 // Routes
 app.use("/videos", videos);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
+    convertVideo("public\\videos\\full\\2024-02-14 21-26-20.mp4", "public\\videos\\transcoded\\folder\\playlist.m3u8", "public\\videos\\transcoded\\folder\\segment%03d.ts")
 });
 
 
