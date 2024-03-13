68cedee9ea49a3b4f0baae3b99b57a212d1525e8
Kuro <quangacon02@gmail.com>
2024-03-13 17:04:48 +0700

    [FE]: Implement Alert Pop up

---
+++ fe/src/components/AlertPopUp.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useState, useEffect, forwardRef } from 'react';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert, { AlertProps } from '@mui/material/Alert';
+
+import { useAlert } from "@/utils/providers/alert";
+
+
+type AlertContextProps = {
+    alertMessage: string;
+    severity?: "success" | "info" | "warning" | "error";
+}
+
+
+const AlertPopUp: React.FC = () => {
+    const [open, setOpen] = useState(false);
+    
+    const { state: { alertMessage, severity } } = useAlert();
+
+    useEffect(() => {
+        if (alertMessage !== "") {
+            setOpen(true);
+        }
+    }, [alertMessage]);
+
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    return (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+            <MuiAlert
+                elevation={6}
+                variant="filled"
+                sx={{ width: '100%' }}
+                severity={severity}
+                onClose={handleClose}
+            >
+                {alertMessage}
+            </MuiAlert>
+        </Snackbar >
+    );
+}
+
+export default AlertPopUp;
---
+++ fe/src/utils/providers/alert.tsx
@@ -0,0 +1,48 @@
+"use client";
+import { useState, useEffect, useMemo, useContext, createContext, Dispatch, SetStateAction } from "react";
+import { type AlertColor } from '@mui/material/Alert';
+
+import AlertPopUp from "@/components/AlertPopUp";
+
+type AlertContextProps = {
+    state: {
+        alertMessage: string;
+        severity?: AlertColor;
+    };
+    dispatch: {
+        setAlertMessage: Dispatch<SetStateAction<string>>;
+        setSeverity: Dispatch<SetStateAction<AlertColor | undefined>>;
+    };
+}
+
+
+export const AlertContext = createContext({} as AlertContextProps);
+
+export const useAlert = () => useContext(AlertContext);
+
+
+const Alert = ({ children }: {
+    children: React.ReactNode
+}) => {
+    const [alertMessage, setAlertMessage] = useState("");
+    const [severity, setSeverity] = useState<AlertColor | undefined>("info");
+
+    // Use memo to prevent unnecessary re-render for the Provider
+    const value = useMemo(() => ({
+        state: {
+            alertMessage: alertMessage,
+            severity: severity,
+        },
+        dispatch: { setAlertMessage, setSeverity },
+    }), [alertMessage, severity]);
+
+    return (
+        <AlertContext.Provider value={value}>
+            {children}
+            <AlertPopUp />
+        </AlertContext.Provider>
+    );
+};
+
+
+export default Alert;
