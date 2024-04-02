217d326f64b3e6dacd569c417ebd34839ceaa624
Kuro <quangacon02@gmail.com>
2024-04-02 23:53:10 +0700

    [BE]: Create film schema

---
+++ be/src/utils/database.ts
@@ -0,0 +1,44 @@
+import { MongoClient, WithId } from "mongodb";
+
+import { MONGO_URI } from "./env.js";
+
+const DBClient = new MongoClient(MONGO_URI);
+
+const DB = DBClient.db("MyAnamel");
+
+
+const DBCategory = DB.collection<{
+    slug: string;
+    name: string;
+}>("categories");
+
+const DBFilm = DB.collection<{
+    slug: string;
+    name: string;
+    originName: string;
+    categoryID: string[];
+    description: string;
+    status: "ongoing" | "completed" | "upcoming" | "cancelled";
+    currentEpisode: number;
+    createdAt: number;
+    updatedAt: number;
+    totalEpisode: number;
+    thumbnail: string;
+    poster: string;
+    trailer: string;
+    subLang: string;
+    views: number;
+    rating: number;
+    year: number;
+    duration: number;
+    episodes: {
+        serverName: string;
+        data: {
+            slug: string;
+            m3u8Link: string;
+        }[]
+    }[]
+}>("films");
+
+
+export { DBCategory, DBFilm };
---
+++ be/src/utils/env.ts
@@ -0,0 +1,5 @@
+const MONGO_URI = process.env.MONGO_URI || 'not now';
+
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+
+export { MONGO_URI, FRONTEND_URL };
