d82d83a15f7a15ab6d791d4127a2245ce8ae7cf3
Vo Hoang Tuan Kiet <vhtkiet9615@gmail.com>
2024-05-18 10:21:18 +0700

    [BE]: add try catch to DB method

--- be/src/api/film.ts
+++ be/src/api/film.ts
@@ -8,6 +8,7 @@ const add = (req: Request, res: Response) => {
 };
 
 const listFilm = async (req: Request, res: Response, next: NextFunction) => {
+    let films;
     let pipeline = [
         {
             $limit: 10,
@@ -22,13 +23,23 @@ const listFilm = async (req: Request, res: Response, next: NextFunction) => {
         },
     ];
 
-    const films = await DBFilm.aggregate(pipeline).toArray();
+    try {
+        films = await DBFilm.aggregate(pipeline).toArray();
+    } catch (err) {
+        console.log(err);
+
+        return next({
+            statusCode: 500,
+            message: "Internal server error",
+        });
+    }
 
     res.status(200).send(films);
 }
 
 const specificFilm = async (req: Request, res: Response, next: NextFunction) => {
     if (req.params.slug) {
+        let film;
         let pipeline = [
             {
                 $match: {
@@ -48,17 +59,32 @@ const specificFilm = async (req: Request, res: Response, next: NextFunction) => 
             },
         ];
 
-        const film = await DBFilm.aggregate(pipeline).toArray();
+        try {
+            film = await DBFilm.aggregate(pipeline).toArray();
+        } catch (err) {
+            console.log(err);
+
+            return next({
+                statusCode: 500,
+                message: "Internal server error",
+            });
+        }
 
         // If film not found, return 400
         if (!film[0]) {
-           return next({
+            return next({
                 statusCode: 400,
                 message: "Film not found",
             });
         }
 
         res.status(200).json(film[0]);
+    }
+    else {
+        next({
+            statusCode: 400,
+            message: "Film not found",
+        });
     }
 };
 
