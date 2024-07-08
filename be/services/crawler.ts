4745113a571bc6d839cf25eaa04bab8caaaa105f
Vo Hoang Tuan Kiet <vhtkiet9615@gmail.com>
2024-07-08 08:51:36 +0700

    [BE]: Modify crawler

--- be/services/crawler.ts
+++ be/services/crawler.ts
@@ -1,5 +1,7 @@
 import { DBCategory, DBFilm } from "../src/utils/database.js"
 
+import { OPHIM_URL } from "../src/utils/env.js";
+
 
 type OPhimMain = {
     status: boolean;
@@ -89,95 +91,76 @@ const ophim1 = async () => {
     const allCategories = await DBCategory.find({}).toArray();
 
     for (let i = 1; i <= 8 && done === false; i++) {
-        let data = await fetch(`https://ophim1.com/danh-sach/phim-moi-cap-nhat?page=${i}`);
-        const main = await data.json() as OPhimMain;
+        try {
+            let res = await fetch(`${OPHIM_URL}/danh-sach/phim-moi-cap-nhat?page=${i}`);
+            const list = await res.json() as OPhimMain;
 
-        if (main.items.length === 0) {
-            console.log("Finished crawling(Out of films)");
-            done = true;
-            break;
-        }
+            if (list.items.length === 0) {
+                console.log("Finished crawling(Out of films)");
+                done = true;
+                break;
+            }
 
-        for (let item of main.items) {
-            data = await fetch(`https://ophim1.com/phim/${item.slug}`);
-            const film = await data.json() as OPhimSpecificFilm;
+            for (let item of list.items) {
+                try {
+                    res = await fetch(`${OPHIM_URL}/phim/${item.slug}`);
+                    const film = await res.json() as OPhimSpecificFilm;
 
-            if (!film.movie.country[1] && film.movie.country[0].slug === "nhat-ban" && film.movie.type === "hoathinh") {
-                // Prepare status for insertion or update
-                done = false;
+                    if (!film.movie.country[1] && film.movie.country[0].slug === "nhat-ban" && film.movie.type === "hoathinh") {
+                        // Prepare status for insertion or update
+                        done = false;
 
-                const episode_total = parseInt(film.movie.episode_total);
-                const totalEpisode = Number.isNaN(episode_total) ? Infinity : parseInt(film.movie.episode_total);
-                const currentEpisode = film.movie.episode_current.match(/Full|Hoàn tất/i) === null ? parseInt(film.movie.episode_current.split("Tập ")[1]) : totalEpisode;
-                const categories: string[] = [];
-                let hasSciFi = false;
+                        const episode_total = parseInt(film.movie.episode_total);
+                        const totalEpisode = Number.isNaN(episode_total) ? Infinity : parseInt(film.movie.episode_total);
+                        const currentEpisode = film.movie.episode_current.match(/Full|Hoàn tất/i) === null ? parseInt(film.movie.episode_current.split("Tập ")[1]) : totalEpisode;
+                        const categories: string[] = [];
+                        let hasSciFi = false;
 
-                film.movie.category.forEach((category) => {
-                    // If has sci-fi category, don't add khoa hoc or vien tuong
-                    if ((category.name.toUpperCase() === "KHOA HỌC" || category.name.toUpperCase() === "VIỄN TƯỞNG") && hasSciFi) return;
+                        film.movie.category.forEach((category) => {
+                            // If has sci-fi category, don't add khoa hoc or vien tuong
+                            if ((category.name.toUpperCase() === "KHOA HỌC" || category.name.toUpperCase() === "VIỄN TƯỞNG") && hasSciFi) return;
 
-                    const match = allCategories.find((cat) => cat.name.toUpperCase().includes(category.name.toUpperCase()));
-                    if (match) {
-                        if (match.slug === "sci-fi") hasSciFi = true;
-                        categories.push(match.slug);
-                    }
-                });
+                            const match = allCategories.find((cat) => cat.name.toUpperCase().includes(category.name.toUpperCase()));
+                            if (match) {
+                                if (match.slug === "sci-fi") hasSciFi = true;
+                                categories.push(match.slug);
+                            }
+                        });
 
-                const payload = {
-                    status: film.movie.status === "trailer" ? "upcoming" : film.movie.status as "ongoing" | "completed" | "upcoming" | "cancelled",
-                    totalEpisode,
-                    currentEpisode,
-                    categories,
-                }
+                        const payload = {
+                            status: film.movie.status === "trailer" ? "upcoming" : film.movie.status as "ongoing" | "completed" | "upcoming" | "cancelled",
+                            totalEpisode,
+                            currentEpisode,
+                            categories,
+                        }
 
-                // Find the film in the database
-                const match = await DBFilm.findOne({ slug: film.movie.slug });
+                        // Find the film in the database
+                        const match = await DBFilm.findOne({ slug: film.movie.slug });
 
-                console.log("Crawled film", film.movie.slug);
-                console.log("New", !match);
-                if (match) {
-                    console.log("Will Updated", match.updatedAt < new Date(film.movie.modified.time))
-                }
+                        console.log("Crawled film", film.movie.slug);
+                        console.log("New", !match);
+                        if (match) {
+                            console.log("Will Updated", match.updatedAt < new Date(film.movie.modified.time))
+                        }
 
-                // Check if film already exists, if not insert it
-                if (!match) {
-                    await DBFilm.insertOne({
-                        slug: film.movie.slug,
-                        name: film.movie.name,
-                        originName: film.movie.origin_name,
-                        description: film.movie.content,
-                        createdAt: new Date(film.movie.created.time),
-                        updatedAt: new Date(film.movie.modified.time),
-                        thumbnail: film.movie.thumb_url,
-                        poster: film.movie.poster_url,
-                        trailer: film.movie.trailer_url,
-                        subLang: film.movie.lang,
-                        views: 0,
-                        rating: 0,
-                        rateCount: 0,
-                        year: film.movie.year,
-                        duration: parseInt(film.movie.time),
-                        episodes: film.episodes.map((episode) => ({
-                            serverName: episode.server_name,
-                            data: episode.server_data.map((data) => ({
-                                slug: data.slug,
-                                name: data.name,
-                                m3u8Link: data.link_m3u8,
-                            })),
-                        })),
-                        ...payload,
-                    });
-                } else {
-                    // Else if exist and isn't up to date, update the database
-                    if (match.updatedAt < new Date(film.movie.modified.time)) {
-                        await DBFilm.updateOne({ slug: film.movie.slug }, {
-                            $set: {
+                        // Check if film already exists, if not insert it
+                        if (!match) {
+                            await DBFilm.insertOne({
+                                slug: film.movie.slug,
+                                name: film.movie.name,
+                                originName: film.movie.origin_name,
+                                description: film.movie.content,
+                                createdAt: new Date(film.movie.created.time),
                                 updatedAt: new Date(film.movie.modified.time),
-                                // This regex might perform poorly (delete "tất" and i flag to optimize)
                                 thumbnail: film.movie.thumb_url,
                                 poster: film.movie.poster_url,
                                 trailer: film.movie.trailer_url,
                                 subLang: film.movie.lang,
+                                views: 0,
+                                rating: 0,
+                                rateCount: 0,
+                                year: film.movie.year,
+                                duration: parseInt(film.movie.time),
                                 episodes: film.episodes.map((episode) => ({
                                     serverName: episode.server_name,
                                     data: episode.server_data.map((data) => ({
@@ -187,16 +170,46 @@ const ophim1 = async () => {
                                     })),
                                 })),
                                 ...payload,
-                            },
-                        });
-                    } else {
-                        // Else stop crawling   
-                        console.log("Finished crawling (film existed)");
-                        done = true;
-                        break;
+                            });
+                        } else {
+                            // Else if exist and isn't up to date, update the database
+                            if (match.updatedAt < new Date(film.movie.modified.time)) {
+                                await DBFilm.updateOne({ slug: film.movie.slug }, {
+                                    $set: {
+                                        updatedAt: new Date(film.movie.modified.time),
+                                        // This regex might perform poorly (delete "tất" and i flag to optimize)
+                                        thumbnail: film.movie.thumb_url,
+                                        poster: film.movie.poster_url,
+                                        trailer: film.movie.trailer_url,
+                                        subLang: film.movie.lang,
+                                        episodes: film.episodes.map((episode) => ({
+                                            serverName: episode.server_name,
+                                            data: episode.server_data.map((data) => ({
+                                                slug: data.slug,
+                                                name: data.name,
+                                                m3u8Link: data.link_m3u8,
+                                            })),
+                                        })),
+                                        ...payload,
+                                    },
+                                });
+                            } else {
+                                // Else stop crawling   
+                                console.log("Finished crawling (film existed)");
+                                done = true;
+                                break;
+                            }
+                        }
                     }
+                } catch (err) {
+                    console.error("Err when crawling", item.slug, err);
+                    done = true;
+                    break;
                 }
             }
+        } catch (err) {
+            console.error("Err when crawling page", i, err);
+            break;
         }
     }
 };
--- be/src/utils/env.ts
+++ be/src/utils/env.ts
@@ -1,7 +1,13 @@
+const ENV = process.env.NODE_ENV || 'development';
+
 const MONGO_URI = process.env.MONGO_URI || 'not now';
 
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 const CURRENT_SEASON = parseInt(process.env.CURRENT_SEASON || "1") || 1;
 
-export { MONGO_URI, FRONTEND_URL, CURRENT_SEASON };
+const SECRET_KEY = process.env.SECRET_KEY || '';
+
+const OPHIM_URL = process.env.OPHIM_URL || 'https://ophim1.com';
+
+export { ENV, MONGO_URI, FRONTEND_URL, CURRENT_SEASON, SECRET_KEY, OPHIM_URL };
